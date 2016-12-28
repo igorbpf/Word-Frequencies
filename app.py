b78a81aa1e6878a8398ad00fdf35e3e63dc831e8
Igor Santos <igorbpf@gmail.com>
2016-12-27 23:05:51 -0200

    Add database instance to app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+from models import Result
 
 @app.route('/')
 def hello():
